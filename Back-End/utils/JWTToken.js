369ea66d70c9279755b3686aaaa625a0cdece52b
TokaKaram <tokakaram@al-arcade.com>
2025-12-09 00:51:43 +0200

    repair .env

--- Back-End/utils/JWTToken.js
+++ Back-End/utils/JWTToken.js
@@ -1,5 +1,6 @@
 const jwt = require("jsonwebtoken");
 module.exports = async (payload) => {
+  // console.log("JWTTOKEN", process.env.JWTTOKEN);
   const token = await jwt.sign(payload, process.env.JWTTOKEN, {
     expiresIn: "4m",
   });

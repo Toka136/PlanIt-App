c2ca2b5fba96c1fe89a96145b62df69ea8a9de3f
TokaKaram <tokakaram@al-arcade.com>
2025-12-09 00:00:45 +0200

    register

--- Front-End/front_end/src/components/Register.jsx
+++ Front-End/front_end/src/components/Register.jsx
@@ -2,27 +2,66 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {faEnvelope,faLock,faUser} from "@fortawesome/free-solid-svg-icons";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useRegisterMutation } from "../API/authSlice";
+import { useState } from "react";
 function Register ()
 {
+  const [Register_user]=useRegisterMutation();
+  const [imgUrl,setImageUrl]=useState('');
+  const navigate=useNavigate()
     const registerSchema = Yup.object({
     FullName: Yup.string().required("FullName is required"),
     Email: Yup.string().required("Email is required"),
     passWord: Yup.string().required("Password is required").min(8,"Password at least 8 charcters"),
     confirmPassword: Yup.string().required("confirmPassword is required").oneOf([Yup.ref('passWord')],"Passwords must match"),
+    avatar:Yup.mixed()
+   
   });
-  const loginFormik = useFormik({
+  const registerFormik = useFormik({
     initialValues: {
     FullName:"",
       Email: "",
       passWord: "",
-      confirmPassword:""
+      confirmPassword:"",
+      avatar:null
     },
     validationSchema: registerSchema,
     onSubmit: (values) => {
       console.log("values=>", values);
+      register(values)
     },
   });
+   const handleFile=(event)=>
+  {
+   const file=event.currentTarget.files[0];
+   console.log('file',file)
+    registerFormik.setFieldValue('avatar',file)
+    const url=URL.createObjectURL(file);
+    setImageUrl(url)
+
+  }
+  const register=async (values)=>
+  {
+    const user=new FormData()
+    user.append('userName',values.FullName);
+    user.append('email',values.Email);
+    user.append('password',values.passWord);
+    user.append('avatar',values.avatar);
+    try
+    {
+      const response= await Register_user(user).unwrap();
+      console.log("response",response)
+       navigate('/login')
+    }catch(err){
+      console.log("error",err)
+    }
+
+     
+
+
+  }
+ 
   return (
     <div className="Auth">
       <div className="Auth_cont">
@@ -35,7 +74,7 @@ function Register ()
           </p>
         </div>
 
-        <form onSubmit={loginFormik.handleSubmit}>
+        <form onSubmit={registerFormik.handleSubmit}>
               <div className="mb-3  form_input">
             <label htmlFor="fullname" className="form-label">
               FullName
@@ -47,16 +86,16 @@ function Register ()
                 id="fullname"
                
                 name="FullName"
-                onChange={loginFormik.handleChange}
-                onBlur={loginFormik.handleBlur}
-                value={loginFormik.values.FullName}
+                onChange={registerFormik.handleChange}
+                onBlur={registerFormik.handleBlur}
+                value={registerFormik.values.FullName}
               />
             </div>
 
             
-            {loginFormik.errors.FullName && loginFormik.touched.FullName ? (
+            {registerFormik.errors.FullName && registerFormik.touched.FullName ? (
               <div className="text-danger small">
-                {loginFormik.errors.FullName}
+                {registerFormik.errors.FullName}
               </div>
             ) : null}
           </div>
@@ -71,18 +110,18 @@ function Register ()
                 id="exampleInputEmail1"
                 aria-describedby="emailHelp"
                 name="Email"
-                onChange={loginFormik.handleChange}
-                onBlur={loginFormik.handleBlur}
-                value={loginFormik.values.Email}
+                onChange={registerFormik.handleChange}
+                onBlur={registerFormik.handleBlur}
+                value={registerFormik.values.Email}
               />
             </div>
 
             <div id="emailHelp" className="form-text">
               We'll never share your email with anyone else.
             </div>
-            {loginFormik.errors.Email && loginFormik.touched.Email ? (
+            {registerFormik.errors.Email && registerFormik.touched.Email ? (
               <div className="text-danger small">
-                {loginFormik.errors.Email}
+                {registerFormik.errors.Email}
               </div>
             ) : null}
           </div>
@@ -96,14 +135,14 @@ function Register ()
                 type="password"
                 name="passWord"
                 id="passWord"
-                onChange={loginFormik.handleChange}
-                onBlur={loginFormik.handleBlur}
-                value={loginFormik.values.passWord}
+                onChange={registerFormik.handleChange}
+                onBlur={registerFormik.handleBlur}
+                value={registerFormik.values.passWord}
               />
             </div>
-            {loginFormik.errors.passWord && loginFormik.touched.passWord ? (
+            {registerFormik.errors.passWord && registerFormik.touched.passWord ? (
               <div className="text-danger small">
-                {loginFormik.errors.passWord}
+                {registerFormik.errors.passWord}
               </div>
             ) : null}
           </div>
@@ -117,18 +156,34 @@ function Register ()
                 type="password"
                 name="confirmPassword"
                 id="confirmPassword"
-                onChange={loginFormik.handleChange}
-                onBlur={loginFormik.handleBlur}
-                value={loginFormik.values.confirmPassword}
+                onChange={registerFormik.handleChange}
+                onBlur={registerFormik.handleBlur}
+                value={registerFormik.values.confirmPassword}
               />
             </div>
-            {loginFormik.errors.confirmPassword && loginFormik.touched.confirmPassword ? (
+            {registerFormik.errors.confirmPassword && registerFormik.touched.confirmPassword ? (
               <div className="text-danger small">
-                {loginFormik.errors.confirmPassword}
+                {registerFormik.errors.confirmPassword}
               </div>
             ) : null}
           </div>
-
+           <div className="profil_avatar">
+            <p>Profile Picture</p>
+            <div className="profil_avatar_cont">
+              <img src={imgUrl?imgUrl:"default.jpg"}/>
+             <div className="text_avatar">
+              <label htmlFor="avatar">add avatar</label>
+              <input 
+              id="avatar"
+              onChange={handleFile}
+              
+              // value={avatar}
+              type="file"
+              />
+              {/* <p>Click to set your profile picture</p> */}
+              </div>
+            </div>
+            </div> 
           <button type="submit" className="btn btn-primary">
             Create account
           </button>
